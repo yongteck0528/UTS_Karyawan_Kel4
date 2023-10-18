278c7202e385b4988ab1aed82b13026eae323c2a
yongteck0528 <aateck.2002@gmail.com>
2023-10-19 00:02:10 +0700

    navbar done

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <title>Home</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid py-4 bg-body-light">
+      <a class="navbar-brand ps-5" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end me-5" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link mx-3 current-page-nav" href="#">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-3" href="performance.php">PERFORMANCE</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+</body>
+
+</html>
