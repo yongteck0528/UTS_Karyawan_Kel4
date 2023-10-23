603becaaeef11e7035a7cae433fa2379632a6334
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2023-10-23 21:25:53 +0700

    updated footer
    footer ada phone number and location, cmn iconnya tidak terlihat.

--- home.php
+++ home.php
@@ -7,88 +7,116 @@
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
-    <title>Home</title>
-  </head>
-  
-  <body>
-    <!-- navbar  -->
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid py-2 bg-body-tertiary">
-        <a class="navbar-brand ps-5" href="#">
-          <img src="image/logo_black.png" alt="" class="img-responsive custom-img">
-        </a>
+
+  <title>Home</title>
+</head>
+
+<body>
+  <!-- navbar  -->
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid py-2 bg-body-tertiary">
+      <a class="navbar-brand ps-5" href="#">
+        <img src="image/logo_black.png" alt="" class="img-responsive custom-img">
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end me-5" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link mx-3 current-page-nav" href="home.php">HOME</a>
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end me-5" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link mx-3 current-page-nav" href="home.php">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-3" href="performance.php">PERFORMANCE</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- section 1 -->
+  <section class="container h-auto">
+
+  </section>
+
+  <!-- section 2 table karyawan tetap -->
+  <section class="container h-auto">
+
+  </section>
+
+  <!-- section 3 table karyawan tidak tetap-->
+  <section class="container h-auto">
+
+  </section>
+
+
+
+  <!-- footer -->
+  <footer class="py-3 my-4 bg-black container-fluid text-light">
+    <div class="row pt-3">
+      <ul class="nav justify-content-center pb-2">
+        <li class="nav-item mx-3">
+          <a href="#" class="nav-link px-2 text-light">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link mx-3" href="performance.php">PERFORMANCE</a>
+        <li class="nav-item mx-3">
+          <a href="#" class="nav-link px-2 text-light">Performance</a>
         </li>
-        
       </ul>
     </div>
-  </div>
-</nav>
-
-<!-- section 1 -->
-<section class="container h-auto">
-
-</section>
-
-<!-- section 2 table karyawan tetap -->
-<section class="container h-auto">
-
-</section>
-
-<!-- section 3 table karyawan tidak tetap-->
-<section class="container h-auto">
-
-</section>
-
-<!-- footer -->
-<footer class="py-3 my-4 bg-black container-fluid text-light">
-  <div class="row pt-3">
-    <ul class="nav justify-content-center pb-2">
-      <li class="nav-item mx-3">
-        <a href="#" class="nav-link px-2 text-light">Home</a>
-      </li>
-      <li class="nav-item mx-3">
-        <a href="#" class="nav-link px-2 text-light">Performance</a>
-      </li>
-    </ul>
-  </div>
-  <hr class="d-flex justify-content-center mx-auto">
-  <div class="row px-5 mx-5">
-    <div class="col pe-5 me-5">
+    <hr class="d-flex justify-content-center mx-auto">
+    <div class="row px-5 mx-5">
+      <div class="col pe-5 me-5">
         <img src="image/logo_white.png" alt="logo" class="img-responsive mx-auto d-block footer-custom-img">
-    </div>
-    <div class="col pe-5 me-5 d-flex flex-column justify-content-center">
-        ISI
-        <br/>
+      </div>
+      <div class="col pe-5 me-5 d-flex flex-column justify-content-center ">
         <div class="row">
+
+          <table border="1">
+            <tr>
+              <td>
+                <img src="icon/marker.png" alt="" style="width: 20px; background-color: white;"> Company Name
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src="icon/phone-call.png" alt="ig" style="width: 20px">(123) 456-7890
+              </td>
+              <td>
+                <img src="icon/fax.png" alt="ig" style="width: 20px">(123) 456-7890
+              </td>
+            </tr>
+
+          </table>
+
+
+
+
+
+
+        </div>
+        ISI
+        <br />
+        <div class="row bg-black">
           <ul class="icon">
-          <li><a href="#"><img src="icon/instagram.png" alt="ig"></a></li>
-          <li><a href="#"><img src="icon/facebook.png" alt="fb"></a></li>
-          <li><a href="#"><img src="icon/linkedin.png" alt="li"></a></li>
-          <li><a href="#"><img src="icon/youtube.png" alt="yt"></a></li>
-          <li><a href="#"><img src="icon/twitter.png" alt="tw"></a></li>
+            <li><a href="#"><img src="icon/instagram.png" alt="ig"></a></li>
+            <li><a href="#"><img src="icon/facebook.png" alt="fb"></a></li>
+            <li><a href="#"><img src="icon/linkedin.png" alt="li"></a></li>
+            <li><a href="#"><img src="icon/youtube.png" alt="yt"></a></li>
+            <li><a href="#"><img src="icon/twitter.png" alt="tw"></a></li>
           </ul>
         </div>
+      </div>
     </div>
-  </div>
-  <div class="row">
-    <p class="text-center text-light">© 2023 Company, Inc</p>
-  </div>
+    <div class="row">
+      <p class="text-center text-light">© 2023 Company, Inc</p>
+    </div>
 
-</footer>
+  </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-..."
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
