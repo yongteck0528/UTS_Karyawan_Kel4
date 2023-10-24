b87f1e978580fc357b839cf7df96ab6dac191631
yongteck0528 <aateck.2002@gmail.com>
2023-10-25 01:45:34 +0700

    added connection php

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+  $server = "localhost";
+  $user = "root";
+  $pass = "";
+  $db = "karyawan_kel4";
+  $con = mysqli_connect($server, $user, $pass, $db);
+
+  if(mysqli_connect_errno()){
+    echo "Koneksi Gagal! ". mysqli_connect_error();
+  }
+
+
+?>
