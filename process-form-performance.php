f7e5622edacb3905b828308ec2809f43fac9bbd0
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2023-10-25 21:18:36 +0700

    FORM SUBMISSION COMPLETE
    WOOOOOOOOOOOOOOOOO

--- process-form-performance.php
+++ process-form-performance.php
@@ -1,7 +1,7 @@
 <?php
 
-$nik = $_POST["nik"];
-$foto = $_FILES["file"]["tmp_name"];
+$nik = (int) $_POST["nik"];
+$loc = $_FILES["file"]["tmp_name"];
 $nama = $_POST["nama"];
 $statusKerja = $_POST["statusKerja"];
 $posisi = $_POST["posisi"];
@@ -11,8 +11,11 @@ $teamwork = $_POST["teamwork"];
 $timeManagement = $_POST["timeManagement"];
 $total = $_POST["total"];
 $grade = $_POST["grade"];
+$filenm = $nama . '-' . uniqid() . '.png';
+move_uploaded_file($loc, 'image/' . $filenm);
 
-// var_dump($nik, $foto, $nama, $statusKerja, $posisi, $tanggalPenilaian, $responsibility, $timeManagement,$teamwork, $total, $grade);
+print_r($_POST);
+// var_dump($nik, $foto, $nama, $statusKerja, $posisi, $tanggalPenilaian, $responsibility, $timeManagement, $teamwork, $total, $grade);
 
 $host = "localhost";
 $dbname = "karyawan";
@@ -26,11 +29,57 @@ $con = mysqli_connect(
     database: $dbname,
 );
 
-if(mysqli_connect_errno()){
-    die("Connection Error:". mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    die("Connection Error:" . mysqli_connect_error());
 }
 
 echo "Connection successful.";
 
+$sql = "INSERT INTO performance (nik, 
+                                foto, 
+                                nama, 
+                                status_kerja, 
+                                position, 
+                                tgl_penilaian, 
+                                responsibility, 
+                                teamwork, 
+                                management_time, 
+                                total, 
+                                grade) 
+        VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+
+$stmt = mysqli_stmt_init($con);
+
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+    die(mysqli_stmt_error($con));
+}
+
+mysqli_stmt_bind_param($stmt,"isssssdddds", 
+                        $nik, 
+                        $filenm, 
+                        $nama, 
+                        $statusKerja, 
+                        $posisi, 
+                        $tanggalPenilaian, 
+                        $responsibility, 
+                        $timeManagement, 
+                        $teamwork, 
+                        $total, 
+                        $grade);
+
+mysqli_stmt_execute($stmt);
+
+echo"The record has been saved";
+// '$nik', 
+// '$filenm', 
+// '$nama', 
+// '$statusKerja', 
+// '$posisi', 
+// '$tanggalPenilaian', 
+// '$responsibility', 
+// '$teamwork', 
+// '$timeManagement', 
+// '$total', 
+// '$grade'
 
 ?>
