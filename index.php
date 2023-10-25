573a37b9c9e7fb8b2aae6bb2efe1bbba59cc6f27
yongteck0528 <aateck.2002@gmail.com>
2023-10-25 23:15:56 +0700

    added login signup

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="log-sign.css">
+  <title>HealthyFruit</title>
+  
+</head>
+
+<body>
+
+  <div class="section">
+    <div class="container">
+      <div class="row full-height justify-content-center">
+        <div class="col-12 text-center align-self-center py-5">
+          <div class="section pb-5 pt-5 pt-sm-2 text-center">
+            <h6 class="mb-0 pb-3"><span>Log In</span><span>Sign Up</span></h6>
+            <input class="checkbox" type="checkbox" name="reg-log" id="reg-log" />
+            <label for="reg-log"></label>
+            <div class="card-3d-wrap mx-auto">
+              <div class="card-3d-wrapper">
+                <div class="card-front">
+                  <div class="center-wrap">
+                    <div class="section text-center">
+                      <h4 class="mb-4 pb-3">Log In</h4>
+                      <div class="form-group">
+                        <input type="email" name="logemail" class="form-style" placeholder="Email" id="logemail"
+                          autocomplete="off">
+                        
+                      </div>
+                      <div class="form-group mt-2">
+                        <input type="password" name="logpass" id="logpass" class="form-style" placeholder="Password"
+                          autocomplete="off">
+                        
+                      </div>
+                      <a href="#" class="btn text-light mt-4">Submit</a>
+                      <p class="mb-0 mt-4 text-center"><a href="#0" class="link">Forgot Your Password?</a></p>
+
+                    </div>
+                  </div>
+                </div>
+                <div class="card-back">
+                  <div class="center-wrap">
+                    <div class="section text-center">
+                      <h4 class="mb-4 pb-3">Sign Up</h4>
+                      <div class="form-group">
+                        <input type="email" name="logemail" class="form-style" placeholder="Email" id="logemail"
+                          autocomplete="off">
+                        
+                      </div>
+                      <div class="form-group mt-2">
+                        <input type="password" name="logpass" id="logpass" class="form-style" placeholder="Password"
+                          autocomplete="off">
+                        
+                      </div>
+                      <a href="#" class="btn text-light mt-4">Submit</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-..."
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
